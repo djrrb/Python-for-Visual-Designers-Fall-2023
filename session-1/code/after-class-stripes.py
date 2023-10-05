59b7a18879afc71639c1c7a958234f95ee95cb1c
David Ross <6578861+djrrb@users.noreply.github.com>
2023-10-05 15:02:16 -0400

    Create after-class-stripes.py

---
+++ session-1/code/after-class-stripes.py
@@ -0,0 +1,11 @@
+myX = 0
+# loop through 10 numbers
+for myNumber in range(10):
+    # draw a rectangle 50 points wide and the full height of the canvas
+    # set the x position to the myX variable
+    # rect(x, y, width, height)
+    rect(myX, 0, 50, height())
+    # augment myX by adding 100 to it every time
+    # this means the rectangles will shift to the right each time
+    # creating stripes
+    myX = myX + 100 # x += 50 
